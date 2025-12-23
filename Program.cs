80bf825e70b0e784f467b980204e8e7b1ab229d0
Carolineps <carolineps083@gmail.com>
2025-12-23 10:53:12 +0100

    created class instance in program.cs and called the ReadFileContent method.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //Create an instance of the class FileHandler. 
+        FileHandler fileHandler = new FileHandler();
+        //Calls the method ReadFileContent and gives the filepath in the current directory.
+        //Method prints in the console the content of the json file (no parsing).
+        fileHandler.ReadFileContent("./datasett.json");
     }
 }
